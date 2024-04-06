7474b849d1f4da39fc34b632eff928eca2cc74db
shubhamthonge <shubhamthonge131@gmail.com>
2024-04-06 11:44:19 +0530

    sec commit

---
+++ src/com/tech/MainClass.java
@@ -0,0 +1,17 @@
+package com.tech;
+
+
+public class MainClass {
+	    public static void main(String[] args) {
+	        int i = 10 + 11 - 12 + 13 - 14 + 15; // Perform arithmetic operations
+	        System.out.println(i); // Output the result
+	    }
+	}
+
+
+//10 + 11 equals 21.
+//21 - 12 equals 9.
+//9 + 13 equals 22.
+//22 - 14 equals 8.
+//8 + 15 equals 23.
+// 23.
