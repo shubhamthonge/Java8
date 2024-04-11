31bc2100a4d4021a30d211d4adb3b3c1f899723b
shubhamthonge <shubhamthonge131@gmail.com>
2024-04-11 11:43:54 +0530

    revers string

---
+++ src/com/tech/Test.java
@@ -0,0 +1,30 @@
+package com.tech;
+
+public class Test {
+
+	public static String revString(String input) {
+
+		String in = input;
+
+		String output = "";
+
+		for (int count = in.length() - 1; count >= 0; count--) {
+			output = output + in.charAt(count);
+
+		}
+		return output;
+
+	}
+
+	public static void main(String[] args) {
+
+		String input = "India is great";
+
+		String output = revString(input);
+		
+		System.out.println(input);
+		System.out.println(output);
+
+
+	}
+}
