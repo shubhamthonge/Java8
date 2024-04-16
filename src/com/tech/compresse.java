f5e8493b35b49f5b7d07e045e80b6abe1b7acd0e
shubhamthonge <shubhamthonge131@gmail.com>
2024-04-16 19:56:48 +0530

    update12

---
+++ src/com/tech/compresse.java
@@ -0,0 +1,22 @@
+package com.tech;
+import java.util.*;
+
+public class compresse {
+    public static String properCompression(String s) {
+        StringBuilder compressedStr = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i += 2) { char c = s.charAt(i); int count = Character.getNumericValue(s.charAt(i + 1)); while (count > 0) {
+                compressedStr.append(c);
+                count--;
+            }
+        }
+
+        return compressedStr.toString();
+    }
+
+    public static void main(String[] args) {
+        String inputStr = "a3b5c2a2";
+        String compressedResult = properCompression(inputStr);
+        System.out.println(compressedResult);  // Output: "aaabbbbbcc"
+    }
+}
