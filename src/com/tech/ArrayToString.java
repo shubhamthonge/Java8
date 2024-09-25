7cd8f8aa1e67c7030c525ce00354ecd106a8072d
shubhamthonge <shubhamthonge131@gmail.com>
2024-09-25 14:05:11 +0530

    string convert

---
+++ src/com/tech/ArrayToString.java
@@ -0,0 +1,11 @@
+package com.tech;
+
+import java.util.Arrays;
+
+public class ArrayToString {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        String arrayAsString = Arrays.toString(arr);
+        System.out.println(arrayAsString);
+    }
+}
