8090e3dfb2350e81808590ca3387accec1bcd328
shubhamthonge <shubhamthonge131@gmail.com>
2024-09-27 12:53:48 +0530

    add

---
+++ src/com/tech/Converts.java
@@ -0,0 +1,10 @@
+package com.tech;
+
+public class Converts {
+
+	public static void main(String[] args) {
+		int num = 123;
+		String str = Integer.toString(num);
+		System.out.println(str);
+	}
+}
