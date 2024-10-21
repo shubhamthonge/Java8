c5c0bf8f60d0f7854d63b4a721673c2b4c4bf5fe
shubhamthonge <shubhamthonge131@gmail.com>
2024-10-21 11:28:17 +0530

    update30

---
+++ src/com/tech/Maina.java
@@ -0,0 +1,15 @@
+package com.tech;
+
+public class Maina {
+
+	
+	    public static void main(String[] args) {
+	        Employee emp = new Employee();
+	        emp.setName("John");
+	        emp.setAge(25);
+
+	        System.out.println("Name: " + emp.getName());
+	        System.out.println("Age: " + emp.getAge());
+	    }
+	}
+
