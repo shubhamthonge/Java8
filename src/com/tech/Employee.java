8c2da8e379fffa836ef71cbf97e0e1b54821042f
shubhamthonge <shubhamthonge131@gmail.com>
2024-10-21 11:24:15 +0530

    update29

---
+++ src/com/tech/Employee.java
@@ -0,0 +1,32 @@
+package com.tech;
+
+public class Employee {
+	 
+	    // Private variables (data hiding)
+	    private String name;
+	    private int age;
+
+	    // Public getter and setter methods
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public int getAge() {
+	        return age;
+	    }
+
+	    public void setAge(int age) {
+	        if (age > 0) {
+	            this.age = age;
+	        } else {
+	            System.out.println("Age must be positive.");
+	        }
+	    }
+	}
+
+
+
