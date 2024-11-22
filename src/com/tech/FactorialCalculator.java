c033df1c9a7b33a99af141dd5e986ad5cbe89f27
shubhamthonge <shubhamthonge131@gmail.com>
2024-11-22 13:28:40 +0530

    add26

--- src/com/tech/FactorialCalculator.java
+++ src/com/tech/FactorialCalculator.java
@@ -6,7 +6,7 @@ public class FactorialCalculator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		// Prompt user for input
+		
 		System.out.print("Enter a positive integer: ");
 		int number = scanner.nextInt();
 
